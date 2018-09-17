06900860ab6c4273e3beec1bb084e43b59337df8
Jack <a89072222@gmail.com>
2018-09-17 19:30:41 +0800

    add is_favorited? method to restaurant model for checking favorite/unfavorite btn & add favorite/unfavorite btn on index page using shared partial

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -11,4 +11,10 @@ class Restaurant < ApplicationRecord
 
   validates_presence_of :name
   mount_uploader :image, PhotoUploader
+
+
+  def is_favorited?(user)
+    self.favorited_users.include?(user)
+  end
+  
 end
